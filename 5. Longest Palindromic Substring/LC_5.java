370a66e0b1a7557e9373308db24fa94ce9aaf5d0
jk2618 <38773092+jk2618@users.noreply.github.com>
2018-10-13 16:35:35 -0500

    Update LC_5.java

--- 5. Longest Palindromic Substring/LC_5.java
+++ 5. Longest Palindromic Substring/LC_5.java
@@ -5,6 +5,7 @@ public class LC_5 {
         }
 
         int length = str.length();
+        // upon initialization, Java sets default values to false
         boolean[][] isPalindrome = new boolean[length][length]; 
         int left = 0;
         int right = 0;
@@ -14,6 +15,8 @@ public class LC_5 {
                 boolean isInnerPalindrome = isPalindrome[i + 1][j - 1] || j - i <= 2;
                 if (str.charAt(i) == str.charAt(j) && isInnerPalindrome) {
                     isPalindrome[i][j] = true;
+                    // this is to memorize the longest length of indexes
+                    // it occurs only when str[i] == str[j] && isInnerPalindrome both are true
                     if (j - i > left - right) {
                         left = i;
                         right = j;
