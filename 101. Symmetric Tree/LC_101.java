023136cd91b6360c76881f79234134b0f3cd656e
jk2618 <38773092+jk2618@users.noreply.github.com>
2018-10-14 15:51:12 -0500

    Create LC_101.java

---
+++ 101. Symmetric Tree/LC_101.java
@@ -0,0 +1,22 @@
+// recursion
+public class LC_101 {
+    public static boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return isMirror(root.left, root.right);
+    }
+
+    public static boolean isMirror(TreeNode node1, TreeNode node2) {
+        if (node1 == null && node2 == null) {
+            return true;
+        }
+        if (node1 == null || node1 == null) {
+            return false;
+        }
+        if (node1.val != node2.val) {
+            return false;
+        }
+        return node1.val == node2.val && isMirror(node1.left, node2.right) && isMirror(node1.right, node2.left);
+    }
+}
