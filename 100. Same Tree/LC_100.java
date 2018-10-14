c22a9ed0d65188ba18e6a80dd3fc3a8aa89bde65
jk2618 <38773092+jk2618@users.noreply.github.com>
2018-10-14 15:47:16 -0500

    Create LC_100.java

---
+++ 100. Same Tree/LC_100.java
@@ -0,0 +1,14 @@
+// recursion
+public class LC_100 {
+    public static boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p != null && q != null) {
+            if (p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
